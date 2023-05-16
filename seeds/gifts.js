dae18fd9ae9fde56ca154ad127e9fd10e4ecdd4c
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-05-16 14:04:39 -0400

    updated seed data

--- seeds/gifts.js
+++ seeds/gifts.js
@@ -1,49 +1,52 @@
-// i need a const something maybe models will check later 
-const { gifts } = require('../models');
+const { Gift } = require('../models');
 
-const gifts = [
-    {
-      name: 'Rose Bouquet',
-      description: 'A beautiful bouquet of roses or any other flower, the only limit on this gift is your imagination ',
-      price: 40.00,
-      category: 'couples, mom',
+const giftSeeds = async () => {
+  try {
+    const giftsData = [
+      {
+        name: 'Rose Bouquet',
+        description: 'A beautiful bouquet of roses or any other flower, the only limit on this gift is your imagination',
+        price: 40.00,
+        gift_category: 'couples',
+      },
+      {
+        name: 'Diamond Necklace',
+        description: 'A brilliant necklace; they are a girl\'s best friend',
+        price: 350.00,
+        gift_category: 'spouse',
+      },
+      {
+        name: 'Teddy Bear',
+        description: 'A cute teddy bear to cuddle with',
+        price: 20.00,
+        gift_category: 'kids',
+      },
+      {
+        name: 'Birthday Card',
+        description: 'A birthday card; simple is typically better, but still write something sentimental',
+        price: 2.99,
+        gift_category: 'all',
+      },
+      {
+        name: 'Gift Card',
+        description: 'Free money used for any and every occasion',
+        price: 50.00,
+        gift_category: 'friend',
+      },
+      {
+        name: 'Watch',
+        description: 'A watch for fashion or to tell time;',
+        price: 175.00,
+        gift_category: 'friend',
+      },
+    ];
 
-    },
-    {
-      name: 'Diamond Necklace',
-      description: 'a brilliant necklace they are a girls best friend ',
-      price: 350.00,
-      category: 'spouse',
-      
-    },
-    {
-      name: 'Teddy Bear',
-      description: 'A cute teddy bear to cuddle with',
-      price: 20.00,
-      category: 'kids, couples',
-      
-    },
-    {
-      name: 'Birthday Card',
-      description: 'a birthday card simple typically better with money in it but still write something sentimental',
-      price: 2.99,
-      category: 'all',
-      
-    },
-    {
-      name: 'Gift Card',
-      description: 'free money used for any and every occasion',
-      price: 50.00,
-      category: 'friend, co-worker',
-      
-    },
-    {
-      name: 'watch',
-      description: 'a watch for fashion or to tell time either way you cant go wrong with a watch',
-      price: 175.00,
-      category: 'friend, co-worker, dad'
+    await Gift.bulkCreate(giftsData);
 
-    },
-  ];
-  //let me know if my category was good or if i need to add anything 
-  module.exports = seedGifts;
+    console.log('Gift are here yayyy!!!!.');
+  } catch (error) {
+    console.error('Error seeding gifts:', error);
+  }
+};
+
+module.exports = giftSeeds;
--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,10 @@
 const sequelize = require('../config/connection');
 const gifts = require('./gifts');
 
-
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedGifts();
+  await gifts.giftSeeds();
 
   process.exit(0);
 };
