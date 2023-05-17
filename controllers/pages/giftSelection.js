cb64a55bb72e88fc10123042f65512a6bac14346
Meghan Simmons <simmons.meghan@gmail.com>
2023-05-17 17:26:51 -0400

    updated code for api route savedgift.js to work for get request

--- controllers/pages/giftSelection.js
+++ controllers/pages/giftSelection.js
@@ -7,8 +7,7 @@ router.get('/', async (req, res) => {
     //because this is an array....need .mpa
     const actualGiftData = giftData.map((gift) => gift.get({ plain: true }));
 
-    const oneGift = actualGiftData[0];
-    
+    const oneGift = actualGiftData[1];
 
     res.render('giftSelection', oneGift);
   });
--- public/js/giftSelection.js
+++ public/js/giftSelection.js
@@ -1,2 +1,17 @@
 console.log("the page is loading!!");
 
+const wishBtnEl = document.getElementById('wish-btn');
+
+
+const formSubmission = async (event) =>{
+    event.preventDefault();
+   
+    const firstName = 'Meghan';
+    console.log(firstName);
+    const lastName = 'Simmons';
+    console.log(lastName);
+    const holidayName = "Valentine's Day";
+    console.log(holidayName);
+}
+
+wishBtnEl.addEventListener('click', formSubmission);
