99924deb9b8896aa3f8f55d7a24e76fc27fa3fed
Meghan Simmons <simmons.meghan@gmail.com>
2023-05-16 23:18:52 -0400

    connected handlebars and now can see in browser

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-
-
-
 const apiRoutes = require('./api');
+const pages = require('./pages')
 
+//localhost:3001/
+router.use('/', pages);
+
+//localhost:3001/api
 router.use('/api', apiRoutes);
-
 
 module.exports = router;
