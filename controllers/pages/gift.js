5df617123e43352404f98b6a880d5f16520c4a08
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-05-17 13:59:30 -0400

    update

--- controllers/pages/gift.js
+++ controllers/pages/gift.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 // included the specific routes to index
 //localhost:3001/
-const giftRoutes = require('./pages/gift');
-router.use('/gift', giftRoutes);
 
 router.get('/', (req, res) => {
-    res.render('gift');
+    res.render('homepage');
 });
 
 router.get('/dashboard', (req, res) =>{
