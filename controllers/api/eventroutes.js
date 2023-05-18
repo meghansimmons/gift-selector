b81f1d5ec4d3f20dcd163512ade88a0ef3535d75
jesus love <snowley777@gmail.com>
2023-05-18 08:29:29 -0500

    made code to get single event attribute

--- controllers/api/eventroutes.js
+++ controllers/api/eventroutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { Event } = require('../../models');
 
-// localhost:3001/api/event
+//localhost:3001/api/event
+
 // router.get('/', async (req, res) => {
 //     try {
 //         const eventData = await Event.findAll({
@@ -11,19 +12,18 @@ const { Event } = require('../../models');
 //         res.status(500).json(err);
 //       }
 //     });
-
-router.get('/', async (req, res) => {
-    try {
-      const eventData = await Event.findOne({
-        where: {
-          holiday_name: req.body.holiday,
-        },
-      });
-      res.status(200).json(eventData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
+ 
+// get a single attribute
+  router.get('/', async (req, res) => {
+  const eventData = await Event.findAll({
+    attributes: [
+      'holiday_name'
+    ]
   });
+      res.status(200).json(eventData);
+   });
+
+
 //post new event
 router.post('/', async (req, res) => {
     try {
